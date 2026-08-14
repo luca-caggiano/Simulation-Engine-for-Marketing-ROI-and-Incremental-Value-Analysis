f5e0b1355e443ef5d4c99b2d62febf8e7e1fe07d
Eredhon <luca2002.caggiano@gmail.com>
2026-08-14 13:38:47 +0200

    fixed json to csv script

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,19 +1,21 @@
 import pandas as pd
 
 # Creating dataframes from json
-portfolio = pd.read_json("Data/raw/portfolio.json", orient='records', lines=True)
-profile = pd.read_json("Data/raw/profile.json", orient='records', lines=True)
-transcript = pd.read_json("Data/raw/transcript.json", orient='records', lines=True)
+portfolio = pd.read_json("Data/raw_json/portfolio.json", orient='records', lines=True)
+profile = pd.read_json("Data/raw_json/profile.json", orient='records', lines=True)
+transcript = pd.read_json("Data/raw_json/transcript.json", orient='records', lines=True)
 
 # Convert channels lists into dummies
 channels_dummies = portfolio["channels"].str.join('|').str.get_dummies('|')
 portfolio_cleaned = pd.concat([portfolio.drop(columns={"channels"}), channels_dummies], axis=1)
 
-# Expand value nested dictionaries into flat table
-value_flat = pd.json_normalize(transcript['value'])
-transcript_cleaned = pd.concat([transcript.drop("value", axis=1), value_flat], axis=1)
+# Expand nested dictionaries in value column to flat table
+transcript["offer id"] = transcript["value"].apply(lambda x : x.get("offer id") or x.get("offer_id")) 
+transcript["reward"] = transcript["value"].apply(lambda x : x.get("reward"))
+transcript["amount"] = transcript["value"].apply(lambda x : x.get("amount"))
+
 
 #Export to csv
-profile.to_csv("Data/processed/profile.csv")
-portfolio_cleaned.to_csv("Data/processed/portfolio.csv")
-transcript_cleaned.to_csv("Data/processed/transcript.csv")
+profile.to_csv("Data/raw_csv/profile.csv", index=False)
+portfolio_cleaned.to_csv("Data/raw_csv/portfolio.csv", index=False)
+transcript.drop("value", axis=1).to_csv("Data/raw_csv/transcript.csv", index=False)
